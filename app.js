b80941bb79c5d881ae4cca5f791ce0aaa7a96462
robert1074004 <abc50113@yahoo.com.tw>
2022-05-08 22:35:45 +0800

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -84,6 +84,14 @@ app.post('/restaurants/:id/edit',(req,res) => {
         .catch(error => console.log(error))
 })
 
+app.post('/restaurants/:id/delete',(req,res) => {
+    const id = req.params.id
+    return Restaurant.findById(id)
+        .then(restaurant => restaurant.remove())
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+})
+
 app.listen(port,() => {
     console.log(`Express is running on http://localhost:${port}`)
 })
