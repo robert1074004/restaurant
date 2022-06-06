5c80ea06151e4b42113e2d00036246daba6bdc3f
robert1074004 <abc50113@yahoo.com.tw>
2022-06-06 10:00:42 +0800

    feat: modify hbs

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const routes = require('./routes')
 
 require('./config/mongoose')
 
-app.engine('handlebars',exphbs({defaultLayout:'main'}))
-app.set('view engine','handlebars')
+app.engine('hbs',exphbs({defaultLayout:'main',extname:'hbs'}))
+app.set('view engine','hbs')
 
 app.use(express.static('public'))
 
