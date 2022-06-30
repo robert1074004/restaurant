ee6c715a9ae39ff0f05bfffe889051d1ce6a4fbf
robert1074004 <abc50113@yahoo.com.tw>
2022-06-30 11:06:17 +0800

    feat: modify restaurantSeeder.js

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,15 +1,93 @@
-
+const bcrypt = require('bcryptjs')
 const Restaurant = require('../restaurant')
+const User = require('../user')
 const restaurants = require('./restaurant.json')
-let mongoose=require('mongoose')
-
+require('dotenv').config()
 const db = require('../../config/mongoose')
+console.log(process.env.MONGODB_URI)
+const SEED_USERS = [{
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678'
+}, {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678'
+}]
 
+db.once('open', () => {
 
-db.once('open',() => {
-    restaurants.results.filter(restaurant => restaurant.id<4 ).forEach(restaurant => {
-        restaurant['userId'] = mongoose.Types.ObjectId("629e1504b276d955c00ca482")
-        Restaurant.create(restaurant)
-    });
-    console.log('done')
+    SEED_USERS.forEach(USER => {
+        bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(USER.password, salt))
+            .then(hash => User.create({
+                name: USER.name,
+                email: USER.email,
+                password: hash
+            }))
+            .then(user => {
+                const userId = user._id
+                const userName = user.name
+                console.log(userName === 'user1')
+                if (userName === "user1") {
+                    const restaurant = restaurants.results.filter(restaurant => restaurant.id < 4)
+                    return Promise.all(Array.from(restaurant, (SEED_RESTAURANT) => {
+                        const {
+                            name,
+                            name_en,
+                            category,
+                            image,
+                            location,
+                            phone,
+                            google_map,
+                            rating,
+                            description
+                        } = SEED_RESTAURANT
+                        Restaurant.create({
+                            name,
+                            name_en,
+                            category,
+                            image,
+                            location,
+                            phone,
+                            google_map,
+                            rating,
+                            description,
+                            userId
+                        })
+
+                    }))
+                }
+                const restaurant = restaurants.results.filter(restaurant => restaurant.id > 3 && restaurant.id < 7)
+                return Promise.all(Array.from(restaurant, (SEED_RESTAURANT) => {
+                    const {
+                        name,
+                        name_en,
+                        category,
+                        image,
+                        location,
+                        phone,
+                        google_map,
+                        rating,
+                        description
+                    } = SEED_RESTAURANT
+                    Restaurant.create({
+                        name,
+                        name_en,
+                        category,
+                        image,
+                        location,
+                        phone,
+                        google_map,
+                        rating,
+                        description,
+                        userId
+                    })
+                }))
+            })
+            .then(() => {
+                console.log('done')
+            })
+    })
 })
